bd78b84a23701b701612d028dba7665687b32155
Chris <none>
2015-01-24 02:03:33 -0500

    hello world

---
+++ src/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+enum class VehicleType
+{
+	Diesel,
+	Electric,
+	Gas
+};
+
+enum class 
+
+int main(int argc, char* argv[])
+{
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
